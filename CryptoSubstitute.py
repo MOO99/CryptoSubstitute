cd1288b4fd61c9d907733e011669317780702c03
sTiKyt <stikyt@protonmail.com>
2020-07-03 17:51:22 +0300

    Fixed "#FIXME" comments to fit code

--- CryptoSubstitute.py
+++ CryptoSubstitute.py
@@ -11,14 +11,14 @@ ClipboardController = ClipboardController()
 if __name__ == "__main__":
     ConsoleView().welcome_message()
     ClipboardController.print_clipboard_content()                                               #FIXME -----------------------------
-    print("======================================================")                             #FIXME          Added this
-    print("Clipboard was printed above for debug purposes! Will be removed in next versions.")  #FIXME      for debug purposes
-    print("======================================================")                             #FIXME -----------------------------
-
-    print(ClipboardController.check_wallet_format("You_can_enter_any_wallet_here"))
+    print("======================================================")                             #FIXME         Added this
+    print("Clipboard was printed above for debug purposes! Will be removed in next versions.")  #FIXME         for debug
+    print("======================================================")                             #FIXME         purposes
+    print(ClipboardController.check_wallet_format("You_can_enter_any_wallet_here"))             #FIXME -----------------------------
 
     #TODO enter your addres, leave blank if you don't want to use this one
     #TODO repeat step from above X times, depending on currency(-ies) you want to use
     #TODO automatically check what type of address user entered and inform him about his choice
     #TODO automatically replace address in clipboard using corresponding replacement address, if available
     #TODO if there are addresses for same currency but of different type - use first available
+
