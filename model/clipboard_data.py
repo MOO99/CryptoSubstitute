2bbbe9116343178d815f0d177cade940b3b63ceb
sTiKyt <stikyt@protonmail.com>
2020-07-03 18:17:45 +0300

    Removed useless code from clipboard_data.py

--- model/clipboard_data.py
+++ model/clipboard_data.py
@@ -1,13 +1,11 @@
 from pyperclip import copy, paste
 
-paste_text = paste()
-
 class ClipboardData:
     def __init__(self):
-        self.clipboard_text_content = paste_text
+        pass
 
     def copy_wallet(self, wallet):
         copy(wallet)
 
     def clipboard_content(self):
-        return self.clipboard_text_content
+        return paste()
