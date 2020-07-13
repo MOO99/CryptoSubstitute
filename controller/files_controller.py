d107c9280bfd6217f11d84aac6d4080d95586694
sTiKyt <stikyt@protonmail.com>
2020-07-13 17:01:46 +0300

    Initial commit for files_controller.py
    Too much to describe

---
+++ controller/files_controller.py
@@ -0,0 +1,17 @@
+from model.files_storage import FilesStorage, WalletsStorage
+
+class WalletsController:
+    def __init__(self):
+        pass
+
+    def confirm_or_create_default_wallets_dir(self):
+        FilesStorage().new_directory()
+
+    def confirm_or_create_default_wallets_csv(self):
+        FilesStorage().new_directory(directory_name='wallets')
+        FilesStorage().enter_directory('wallets')
+        if FilesStorage().check_if_csv_exists('default.csv'):
+            pass
+        else:
+            FilesStorage().new_file('default.csv')
+        FilesStorage().return_to_root_directory()
