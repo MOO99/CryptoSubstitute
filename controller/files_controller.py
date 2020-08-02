7f08ba5cfb58d5b84bbdbcefb7661cac10b29819
sTiKyt <stikyt@protonmail.com>
2020-08-02 12:37:39 +0300

    Fixed confirm_or_create_default_wallets_(dir and csv) methods

--- controller/files_controller.py
+++ controller/files_controller.py
@@ -5,17 +5,21 @@ class WalletsController:
         pass
 
     def confirm_or_create_default_wallets_dir(self):
-        FilesStorage().new_directory()
+        if FilesStorage().check_if_folder_exists("wallets"):
+            pass
+        else:
+            FilesStorage().new_directory('wallets')
 
     def confirm_or_create_default_wallets_csv(self):
-        FilesStorage().new_directory(directory_name='wallets')
         FilesStorage().enter_directory('wallets')
         if FilesStorage().check_if_csv_exists('default.csv'):
             pass
         else:
             FilesStorage().new_file('default.csv')
         FilesStorage().return_to_root_directory()
+
     def divide_csv_content_into_smaller_ones(self): #TODO
         pass
+
     def add_wallet_to_default_csv(self, wallet_to_add: str):
         WalletsStorage().add_wallet_to_csv(csv_path='default.csv', wallet_to_add=wallet_to_add)
