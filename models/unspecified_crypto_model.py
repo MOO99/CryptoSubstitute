6d711678d0e1d8e2bec51abe9fd8590de12739a0
sTiKyt <stikyt@protonmail.com>
2020-08-14 13:54:19 +0300

    Created unspecified_crypto_model

---
+++ models/unspecified_crypto_model.py
@@ -0,0 +1,20 @@
+class UnspecifiedCryptoModel:
+    def __init__(self, target: str = "", replacement: str = ""):
+        self._target = target
+        self._replacement = replacement
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, new_target: str):
+        self._target = new_target
+
+    @property
+    def replacement(self):
+        return self._replacement
+
+    @replacement.setter
+    def replacement(self, new_replacement: str):
+        self._replacement = new_replacement
